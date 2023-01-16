8b89c32b972797962e9c45944936589370e1b1b2
Gabriella <gabriellakiss321@outlook.com>
2023-01-16 21:40:06 +0200

    generate summarize function

---
+++ project/app/summarizer.py
@@ -0,0 +1,20 @@
+from newspaper import Article
+import nltk
+from app.models.tortoise import TextSummary
+
+
+async def generate_summary(summary_id: int, url: str) -> None:
+    article = Article(url)
+    article.download()
+    article.parse()
+
+    try:
+        nltk.data.find("tokenizers/punkt")
+    except LookupError:
+        nltk.download("punkt")
+    finally:
+        article.nlp()
+
+    summary = article.summary
+
+    await TextSummary.filter(id=summary_id).update(summary=summary)
